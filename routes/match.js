b89249d8dc75ba671d5bee3f8b8200707cc509ee
GaborTeichter <66086127+GaborTeichter@users.noreply.github.com>
2021-06-07 15:45:50 +0200

    I created vote.js and fixed controllers

--- routes/match.js
+++ routes/match.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 var match = require('../model/match');
+var teamModel = require('../model/team');
+var voteModel = require('../model/vote');
 
 /* var express = require('express');
 var router = express.Router();
